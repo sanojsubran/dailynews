daf31a1ca2ff0e4f4ae2d839eee064aa10337606
sanoj subran <sanoj009@gmail.com>
2020-05-03 22:57:32 +0530

    Site under construction page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react'
 import Table from './Table.js'
+import axios from 'axios'
+import logo from './logo.jpg';
 
 class App extends Component {
-    render() {
-        const characters = [
-            {
-              name: 'Charlie',
-              job: 'Janitor',
-            },
-            {
-              name: 'Mac',
-              job: 'Bouncer',
-            },
-            {
-              name: 'Dee',
-              job: 'Aspring actress',
-            },
-            {
-              name: 'Dennis',
-              job: 'Bartender',
-            },
-          ]
-      return (
+  state = {
+    persons: []
+  }
+  
+  render() {
+    return (
        <div className="container">
-         <div><h1>REACT app in development</h1></div>
-         <Table characterData={characters} />
+         <p>
+          <img src={logo} alt="Site Under construction" title="Site Under construction" width="800" height="600" border="0"/>
+         </p>
+         <p>
+          <a href="https://www.freepik.com/free-photos-vectors/technology">Technology vector created by upklyak - www.freepik.com</a>
+         </p>
+         
        </div>
       )
     }
