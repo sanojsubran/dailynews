6abdf5a8aaca1f5270245695d18c2d10b8d89cf5
sanoj subran <sanoj009@gmail.com>
2020-07-05 08:54:23 +0530

    Updating release changes to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react'
-import Table from './Table.js'
-import axios from 'axios'
-import logo from './logo.jpg';
 
 class App extends Component {
-  state = {
-    persons: []
-  }
-  
-  render() {
-    return (
-       <div className="container">
-         <p>
-          <img src={logo} alt="Site Under construction" title="Site Under construction" width="800" height="600" border="0"/>
-         </p>
-         <p>
-          <a href="https://www.freepik.com/free-photos-vectors/technology">Technology vector created by upklyak - www.freepik.com</a>
-         </p>
-       </div>
-      )
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      stories: []
     }
   }
+  
+
+  componentDidMount() {
+    fetch("https://api.techdigest.today/")
+    .then(res => res.json())
+    .then(
+      (data) => {
+      this.setState({ 
+        isLoaded:true,
+        stories: data,
+    });
+  },
+  (error) => {
+    this.setState({
+      isLoaded: true,
+      error
+    });
+  })
+}
+  
+  render() {
+    const { error, isLoaded, items } = this.state;
+    this.story = this.state.stories.map((item,key) =>
+    <li><a href={item.url} target="_blank" title={item.title}>{item.title}</a></li>
+    );
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container">
+          <h2>Hacker News</h2>
+          <ul>{this.story}</ul>
+        </div>
+       )
+    }
+  }
+}
 
   export default App
