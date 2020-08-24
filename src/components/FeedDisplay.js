aa84ba7240378ae88e48ab786e32e06ff0ab36b2
sanojsubran <sanoj009@gmail.com>
2020-08-24 12:55:48 +0000

    Adding fix for the multiline issue

--- src/components/FeedDisplay.js
+++ src/components/FeedDisplay.js
@@ -2,6 +2,13 @@ import React from 'react';
 import FeedEntryPlaceholder from './FeedEntryPlaceholder';
 import './CommonStyle.css';
 
+function reduceTitle(title) {
+    var wordArray = title.split(' ');
+    wordArray = wordArray.length > 11 ?wordArray.splice(0,13).join(' ') + '\ ...'
+        : wordArray.join(' ');
+    return wordArray;
+}
+
 const FeedDisplay = ({currentStories}) => {
     if(currentStories.length === 0) {
         return (
@@ -9,10 +16,11 @@ const FeedDisplay = ({currentStories}) => {
         );
     }
     const renderedStories = currentStories.map( story => {
+        var title = reduceTitle(story.title);
         return (
             <div key={story.id} className="item news">
                 <div className="ui text feedEntry ">
-                    <a href={story.url} target="blank" rel="noopener noreferrer">{story.title}</a>
+        <a href={story.url} target="blank" rel="noopener noreferrer ">{title}</a>
                 </div>
             </div>
         );
